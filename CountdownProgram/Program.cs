6bec13e0ea80a7c93b73062caf05ad70b5d5a85c
jkpdominguez-boop <jkpdominguez@pcu.edu.ph>
2026-03-03 23:34:30 +0800

    Initial commit

---
+++ CountdownProgram/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+ namespace CountdownProgram
+  {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Enter the number of seconds for the countdown:");
+                int seconds;
+                if (int.TryParse(Console.ReadLine(), out seconds) && seconds > 0)
+                {
+                    Countdown(seconds);
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid positive integer.");
+                }
+            }
+    
+            static void Countdown(int seconds)
+            {
+                for (int i = seconds; i > 0; i--)
+                {
+                    Console.WriteLine(i);
+                    System.Threading.Thread.Sleep(1000); // Wait for 1 second
+                }
+                Console.WriteLine("Countdown complete!");
+            }
+        }
+    }
